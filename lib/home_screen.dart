45271f9b783794efc1df59804455e14a67785134
GregSourget <sourgetgregoire@mail.com>
2025-03-12 06:22:29 +0100

    V1.1 CuistoLoco

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -9,13 +9,14 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   String _recipe = '';
+  bool isLoading = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Accueil'),
-        automaticallyImplyLeading: false, // Désactiver la flèche de retour
+        automaticallyImplyLeading: false,
       ),
       body: SingleChildScrollView(
         padding: EdgeInsets.all(16.0),
@@ -24,7 +25,7 @@ class _HomeScreenState extends State<HomeScreen> {
           children: <Widget>[
             Text(
               "Bienvenue sur la page d'accueil !",
-              style: TextStyle(fontSize: 20), // Taille de la police 20
+              style: TextStyle(fontSize: 20),
             ),
             SizedBox(height: 20),
             ElevatedButton(
@@ -38,35 +39,46 @@ class _HomeScreenState extends State<HomeScreen> {
               },
               child: Text(
                 "Scanner votre frigo",
-                style: TextStyle(fontSize: 20), // Taille de la police 20 pour le texte du bouton
+                style: TextStyle(fontSize: 20),
               ),
             ),
             SizedBox(height: 20),
             ElevatedButton(
               onPressed: () async {
-                try {
-                  if (PixtralAPI.ingredients.isEmpty) {
-                    throw Exception('Les ingrédients ne sont pas disponibles. Veuillez d\'abord capturer une image.');
-                  }
+                if (PixtralAPI.ingredients.isEmpty) {
+                  print('Les ingrédients ne sont pas disponibles.');
+                  return;
+                }
 
+                setState(() {
+                  isLoading = true;
+                });
+
+                try {
                   String result = await MistralAPI.getSummary();
                   setState(() {
                     _recipe = result;
+                    isLoading = false;
                   });
                 } catch (e) {
+                  setState(() {
+                    isLoading = false;
+                  });
                   print('Erreur lors de la génération de la recette : $e');
                 }
               },
               child: Text(
                 'Générer une recette',
-                style: TextStyle(fontSize: 20), // Taille de la police 20 pour le texte du bouton
+                style: TextStyle(fontSize: 20),
               ),
             ),
             SizedBox(height: 20),
-            if (_recipe.isNotEmpty)
+            if (isLoading)
+              Center(child: CircularProgressIndicator()),
+            if (!_recipe.isEmpty && !isLoading)
               Text(
                 _recipe,
-                style: TextStyle(fontSize: 20), // Taille de la police 20 pour le texte de la recette
+                style: TextStyle(fontSize: 20),
               ),
           ],
         ),
